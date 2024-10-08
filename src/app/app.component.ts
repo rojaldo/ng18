d747a46bf1d838795e4854fa00c591b05342aba8
Daniel Rojo <danielrojo@gmail.com>
2024-10-08 12:21:29 +0200

    heroes component + @for + NgModel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,19 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CalculatorComponent } from "./components/calculator/calculator/calculator.component";
 import { CalculatorService } from './services/calculator.service';
+import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeroesComponent } from './components/heroes/heroes/heroes.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, JsonPipe, CalculatorComponent],
+  imports: [RouterOutlet, JsonPipe, NgbNavModule, CalculatorComponent, HeroesComponent],
   providers: [CalculatorService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'ng18';
+  active = 'heroes';
 
 }
---
+++ src/app/components/heroes/heroes/heroes.component.ts
@@ -0,0 +1,22 @@
+import { NgFor } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-heroes',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './heroes.component.html',
+  styleUrl: './heroes.component.scss'
+})
+export class HeroesComponent {
+
+  heroes = [ 'Windstorm', 'Bombasto', 'Magneta', 'Tornado' ];
+  heroName = '';
+
+  addHero() {
+    this.heroes.push(this.heroName);
+    this.heroName = '';
+  }
+
+}
